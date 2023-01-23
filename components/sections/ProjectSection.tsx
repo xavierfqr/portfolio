0e5a20ed5b2be0cca03b38279d2a9d8503dcde5f
xavier facqueur <facqueur.xavier@gmail.com>
2023-01-23 18:27:33 +0100

    Add: responsive projects section

--- components/sections/ProjectSection.tsx
+++ components/sections/ProjectSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading, Image, useColorModeValue } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Image, useColorModeValue, useMediaQuery } from '@chakra-ui/react';
 import React, { forwardRef, useEffect, useRef, useState } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { ArrowBackIcon, ArrowForwardIcon, ArrowLeftIcon, ArrowRightIcon, Icon } from '@chakra-ui/icons';
@@ -38,7 +38,8 @@ const projects: Project[] = [
   {
     src: '/projects/GE_Healthcare.png',
     alt: 'Project GE Healthcare',
-    desc: 'Web application built for General Electric Healthcare to help doctors interpreting medical images on their patients',
+    desc: 'a bit too long',
+    // desc: 'Web application built for General Electric Healthcare to help doctors interpreting medical images on their patients',
   },
   {
     src: '/projects/documents.png',
@@ -49,28 +50,32 @@ const projects: Project[] = [
 
 // eslint-disable-next-line react/display-name
 export const ProjectSection = forwardRef<HTMLDivElement, any>((_, ref) => {
-  const [selectedIndex, setSelectedIndex] = useState(0);
   const [carouselWidth, setCarouselWidth] = useState(0);
   const [isArrowDisabled, setIsArrowDisabled] = useState(false);
   const carouselRef = useRef<HTMLDivElement>(null);
   const projectRef = useRef<HTMLDivElement>(null);
   const brandColor = useColorModeValue('#319795', '#50E3C2');
+  const descColor = useColorModeValue('white', 'white');
+  const [isLowerThan720] = useMediaQuery('(max-width: 720px)');
 
   useEffect(() => {
     if (!carouselRef.current || !carouselRef.current.scrollWidth) return;
     setCarouselWidth(carouselRef.current.scrollWidth - carouselRef.current.offsetWidth);
-  }, []);
+  }, [isLowerThan720]);
 
   const onArrowClick = (direction: 'left' | 'right') => {
     if (isArrowDisabled) return;
     if (!projectRef.current || !projectRef.current.offsetWidth) return;
+
     const width = projectRef.current.offsetWidth;
     if (!carouselRef.current || !carouselRef.current.scrollWidth) return;
+
     if (direction === 'left') {
       carouselRef.current.scrollLeft -= width;
     } else {
       carouselRef.current.scrollLeft += width;
     }
+
     setIsArrowDisabled(true);
     setTimeout(() => {
       setIsArrowDisabled(false);
@@ -78,26 +83,51 @@ export const ProjectSection = forwardRef<HTMLDivElement, any>((_, ref) => {
   };
 
   return (
-    <Flex ref={ref} h="100vh" mx="auto" maxWidth="80%" justify="center" direction="column" position="relative">
-      <Heading as="h2" color={brandColor} mb={{ base: 2, md: 20 }} display="flex" alignItems="center" margin="auto">
+    <Flex
+      ref={ref}
+      h="100vh"
+      mx="auto"
+      maxWidth={{ base: '95%', md: '80%' }}
+      justify="center"
+      direction="column"
+      position="relative"
+    >
+      <Heading
+        as="h2"
+        color={brandColor}
+        mb={{ base: 2, md: 20 }}
+        display="flex"
+        alignItems="center"
+        margin="auto"
+        mt={24}
+      >
         <Image src="/spades.png" w={7} h={7} mr={3} alt="spade" />
         Projects
       </Heading>
-      <Box className="flex items-center justify-center">
-        <Icon onClick={() => onArrowClick('left')} bg="whiteAlpha.100" cursor="pointer" rounded="md" boxSize="10">
-          <ArrowBackIcon></ArrowBackIcon>
-        </Icon>
+      <Box className="flex items-center justify-center flex-grow">
+        {!isLowerThan720 && (
+          <Icon onClick={() => onArrowClick('left')} bg="whiteAlpha.100" cursor="pointer" rounded="md" boxSize="10">
+            <ArrowBackIcon></ArrowBackIcon>
+          </Icon>
+        )}
         <motion.div
           ref={carouselRef}
           className="cursor-grab  overflow-hidden rounded-xl bg-gray-800 scroll-smooth"
           whileTap={{ cursor: 'grabbing' }}
         >
-          <motion.div drag="x" dragConstraints={{ right: 0, left: -carouselWidth }} className="flex rounded-xl w-full">
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 0, left: -carouselWidth }}
+            dragListener={isLowerThan720 ? true : false}
+            className="flex rounded-xl w-full"
+          >
             {projects.map((project, index) => (
               <motion.div
                 ref={projectRef}
                 key={index}
-                className={`min-w-[100%] max-w-[100%] p-10 flex flex-col items-center justify-center rounded-l`}
+                className={`min-w-[100%] max-w-[100%] flex flex-col items-center justify-center rounded-l ${
+                  project.url ? 'cursor-pointer' : 'cursor-auto'
+                }`}
               >
                 <Image
                   src={project.src}
@@ -105,16 +135,19 @@ export const ProjectSection = forwardRef<HTMLDivElement, any>((_, ref) => {
                   borderRadius="2xl"
                   onClick={() => (project.url ? window.open(project.url, '_blank') : null)}
                   w="60%"
-                  cursor={`${project.url} ? 'pointer' : 'auto'`}
                 />
-                <p>{[project.desc]}</p>
+                <Box as="i" fontSize="sm" color={descColor} mt={{ base: 2, md: 10 }}>
+                  {project.desc}
+                </Box>
               </motion.div>
             ))}
           </motion.div>
         </motion.div>
-        <Icon onClick={() => onArrowClick('right')} bg="whiteAlpha.100" cursor="pointer" rounded="md" boxSize="10">
-          <ArrowForwardIcon></ArrowForwardIcon>
-        </Icon>
+        {!isLowerThan720 && (
+          <Icon onClick={() => onArrowClick('right')} bg="whiteAlpha.100" cursor="pointer" rounded="md" boxSize="10">
+            <ArrowForwardIcon></ArrowForwardIcon>
+          </Icon>
+        )}
       </Box>
     </Flex>
   );
